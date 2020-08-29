2d53f386d630fb79f2dff661cc62ce931466fcbd
Dale Floer <dfloer@gmail.com>
2020-08-29 00:57:23 -0700

    Added a small city edit demo program.
    It's limited right now, and it is mostly used to testing.

--- Examples & Utilities/city_tools.py
+++ Examples & Utilities/city_tools.py
@@ -5,15 +5,14 @@ sys.path.append('..')
 import sc2_parse as sc2p
 
 
-
-
-
 def parse_command_line():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', dest="input_file", help="input city filename", metavar="FILE", required=True)
     parser.add_argument('-o', '--output', dest="output_file", help="output image filename", metavar="FILE", required=True)
     parser.add_argument('-m', '--money', dest="money", help="amount of money", metavar="NUMBER", required=False)
     parser.add_argument('-t', '--terrain', dest="terrain_mode", help="go into terrain edit mode", required=False, action="store_true")
+    parser.add_argument('-p', '--pause', dest="game_pause", help="pause the game", required=False, action="store_true")
+    parser.add_argument('-n', '--no-sound', dest="mute", help="mute music and fx", required=False, action="store_true")
     args = parser.parse_args()
     return args
 
@@ -24,13 +23,19 @@ if __name__ == "__main__":
     output_file = options.output_file
     money = options.money
     terrain_mode = options.terrain_mode
+    mute = options.mute
 
     city = sc2p.City()
     city.create_city_from_file(input_file)
 
     if money:
-        city.city_attributes["TotalFunds"] = min(money, 2 ** 31 - 1)
+        city.city_attributes["TotalFunds"] = min(int(money), 2 ** 31 - 1)
     if terrain_mode:
         city.simulator_settings["GameMode"] = 0
+    if options.game_pause:
+        city.game_settings["GameSpeed"] = 1
+    if options.mute:
+        city.game_settings["UserSoundOn"] = 0
+        city.game_settings["UserMusicOn"] = 0
 
     city.save_city(output_file)
